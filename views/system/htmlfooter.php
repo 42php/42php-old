058d02762f1bf905c2b459e1a2206803c14620da
Guillaume Gagnaire <contact@scrola.net>
2016-11-09 14:16:40 +0100

    Added the \Core\View class to render views.

---
+++ views/system/htmlfooter.php
@@ -0,0 +1,20 @@
+        <?php foreach (Conf::get('page.js', []) as $script) { ?>
+            <script type="text/javascript" src="<?=$script ?>"></script>
+        <?php } ?>
+        <?php $mod = Conf::get('route.name', ''); $a = Conf::get('page.analytics', ''); if ($a != '' && $mod != 'backoffice') { ?>
+            <script type="text/javascript">
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+                ga('create', '<?=$a ?>', 'auto');
+                ga('require', 'displayfeatures');
+                ga('send', 'pageview');
+            </script>
+        <?php } ?>
+        <?php foreach (Conf::get('page.bottom', []) as $script) { ?>
+            <?=$script ?>
+        <?php } ?>
+    </body>
+</html>
---
+++ views/system/htmlheader.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="<?= Conf::get('lang') ?>">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="robots" content="<?= Conf::get('page.robots') ?>" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title><?= Conf::get('page.title') ?></title>
+        <meta name="description" content="<?= str_replace('"', '&quot;', Conf::get('page.description')) ?>" />
+        <meta name="keywords" content="<?= str_replace('"', '&quot;', Conf::get('page.keywords')) ?>" />
+
+        <link rel="shortcut icon" href="<?= Conf::get('page.favicon', '/favicon.ico') ?>" type="image/x-icon">
+        <link rel="icon" href="<?= Conf::get('page.favicon', '/favicon.ico') ?>" type="image/x-icon">
+
+        <?php foreach (Conf::get('page.meta', []) as $line) { ?>
+            <?= $line ?>
+        <?php } ?>
+
+        <!-- Facebook share -->
+        <meta property="og:title" content="<?= str_replace('"', '&quot;', Conf::get('page.title')) ?>">
+        <?php if (Conf::get('page.share.image', '') != '') { ?>
+            <meta property="og:image" content="<?= str_replace('"', '&quot;', Conf::get('page.share.image')) ?>">
+        <?php } ?>
+        <meta property="og:description" content="<?= str_replace('"', '&quot;', Conf::get('page.description')) ?>">
+
+        <!-- Twitter share -->
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="<?= Conf::get('page.share.twitter.username') ?>" />
+        <meta name="twitter:title" content="<?= str_replace('"', '&quot;', Conf::get('page.title')) ?>" />
+        <meta name="twitter:description" content="<?= str_replace('"', '&quot;', Conf::get('page.description')) ?>" />
+        <?php if (Conf::get('page.share.image', '') != '') { ?>
+            <meta name="twitter:image" content="<?= str_replace('"', '&quot;', Conf::get('page.share.image')) ?>" />
+        <?php } ?>
+
+        <?php
+        // alternate links for multilingual
+        if (Conf::get('route') !== false) {
+            foreach (i18n::$__acceptedLanguages as $l) {
+                $url = Argv::createUrl(Conf::get('route.name'), Conf::get('route.params'), $l);
+                echo '<link rel="alternate" hreflang="'.$l.'" href="http://'.$_SERVER['HTTP_HOST'].$url.'" />'."\n";
+            }
+        }
+        ?>
+
+        <?php foreach (Conf::get('page.css', []) as $file) { ?>
+            <link rel="stylesheet" type="text/css" href="<?=$file ?>" />
+        <?php } ?>
+    </head>
+    <body>
