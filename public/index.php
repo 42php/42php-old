dd2d221793bf59df230c69aaba2818cf0a8c9fb4
Guillaume Gagnaire <contact@scrola.net>
2016-11-14 13:58:30 +0100

    Appel du contrôleur

--- public/index.php
+++ public/index.php
@@ -4,3 +4,24 @@
  * Call the initialization script
  */
 include '../scripts/init.php';
+
+$routes = \Core\JSON::toArray(ROOT.'/config/routes.json');
+
+\Core\Conf::set('route', false);
+$route = \Core\Argv::route($argv, $routes);
+if (isset($route['route']))
+    \Core\Conf::set('route', $route['route']);
+
+\Core\Session::$apiMode = $route && isset($route['conf']['api']) && $route['conf']['api'];
+\Core\Session::init();
+
+include ROOT.'/scripts/i18n.php';
+
+if (!$route) {
+    $route = [
+        'controller' => 'RootController@redirect',
+        'params' => ''
+    ];
+}
+
+echo \Core\Controller::run($route['controller'], $route['params']);
