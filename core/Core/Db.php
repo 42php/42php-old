7540287ace41ec81db331bc8e0916176e0ee4049
Guillaume Gagnaire <alduro.estallas@gmail.com>
2016-11-14 23:05:32 +0100

    Appel de la factory

---
+++ core/Core/Db.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace                           Core;
+
+/**
+ * Gère les requêtes sur la base de données
+ *
+ * Class Db
+ * @package Core
+ */
+class                               Db {
+    /**
+     * Appelle la Factory du driver configuré. (\Drivers\Database\nomDuDriver\Factory::getInstance())
+     *
+     * @return mixed                L'instance de base de données
+     */
+    public static function          getInstance() {
+        $factory = '\Drivers\Database\\'. Conf::get('database.type', 'PDO') .'\\Factory';
+        if (class_exists($factory))
+            return $factory::getInstance();
+        return false;
+    }
+}
