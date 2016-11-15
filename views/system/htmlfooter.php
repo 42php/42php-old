8de3bc6e45a9794a2698d832ffaeb78d7e9bd2a4
Guillaume Gagnaire <alduro.estallas@gmail.com>
2016-11-15 22:00:01 +0100

    Fix header et footer

--- views/system/htmlfooter.php
+++ views/system/htmlfooter.php
@@ -1,7 +1,7 @@
-        <?php foreach (Conf::get('page.js', []) as $script) { ?>
+        <?php foreach (\Core\Conf::get('page.js', []) as $script) { ?>
             <script type="text/javascript" src="<?=$script ?>"></script>
         <?php } ?>
-        <?php $mod = Conf::get('route.name', ''); $a = Conf::get('page.googleAnalytics', ''); if ($a != '') { ?>
+        <?php $mod = \Core\Conf::get('route.name', ''); $a = \Core\Conf::get('page.googleAnalytics', ''); if ($a != '') { ?>
             <script type="text/javascript">
                 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -13,7 +13,7 @@
                 ga('send', 'pageview');
             </script>
         <?php } ?>
-        <?php foreach (Conf::get('page.bottom', []) as $script) { ?>
+        <?php foreach (\Core\Conf::get('page.bottom', []) as $script) { ?>
             <?=$script ?>
         <?php } ?>
     </body>
