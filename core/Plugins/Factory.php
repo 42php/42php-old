f6014ea6e2b072ecd40a42b15c3d33dc8b0fcec7
Guillaume Gagnaire <alduro.estallas@gmail.com>
2016-11-19 15:03:32 +0100

    Ajout de l'instance Factory

---
+++ core/Plugins/Factory.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * LICENSE: This source file is subject to version 3.0 of the GPL license
+ * that is available through the world-wide-web at the following URI:
+ * https://www.gnu.org/licenses/gpl-3.0.fr.html (french version).
+ *
+ * @author      Guillaume Gagnaire <contact@42php.com>
+ * @link        https://www.github.com/42php/42php
+ * @license     https://www.gnu.org/licenses/gpl-3.0.fr.html GPL
+ */
+
+namespace                   Plugins;
+
+/**
+ * Gère un plugin
+ *
+ * Interface Factory
+ * @package Plugins
+ */
+interface                   Factory {
+    public static function  getInstance();
+    public function         render();
+}
