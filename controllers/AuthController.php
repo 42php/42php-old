cf950250a7620ac8d847830ba2f32bde1ef8a192
Guillaume Gagnaire <alduro.estallas@gmail.com>
2016-11-19 12:01:37 +0100

    Base du controlleur Auth

---
+++ controllers/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Class AuthController
+ */
+class                   AuthController extends \Core\Controller {
+    public function     login() {
+        if (isset($_POST['email'], $_POST['password'])) {
+
+        }
+
+
+    }
+}
