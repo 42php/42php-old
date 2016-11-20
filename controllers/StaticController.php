0362f679622c9ed8a41fc990387f5bafbdfcbd3a
Guillaume Gagnaire <alduro.estallas@gmail.com>
2016-11-20 18:39:40 +0100

    Si /, redirection vers la home

--- controllers/StaticController.php
+++ controllers/StaticController.php
@@ -7,4 +7,8 @@ class                   StaticController extends \Core\Controller {
     public function     home() {
         return \Core\View::render('home');
     }
+
+    public function     redirectToLang() {
+        \Core\Redirect::http(\Core\Argv::createUrl('home'));
+    }
 }
