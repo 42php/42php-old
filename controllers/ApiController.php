ee452f17b9805073947f4c5ff06fbf2fd2df0250
Guillaume Gagnaire <contact@scrola.net>
2016-11-30 22:23:41 +0100

    Controlleur de l'API

---
+++ controllers/ApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Class ApiController
+ */
+class                   ApiController extends \Core\Controller {
+    use                 Api_Auth;
+
+    public function     process() {
+        $methods = get_class_methods($this);
+        foreach ($methods as $method) {
+            if (in_array($method, ['process', '__construct', 'run', 'exists']))
+                continue;
+            $this->$method();
+        }
+
+        \Core\Api::run(1);
+    }
+}
