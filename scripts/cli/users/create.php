8d9deb89b3d4e7db2bdea2d6b2622553944b8ddf
Guillaume Gagnaire <contact@scrola.net>
2016-12-01 00:18:35 +0100

    Script de création d'utilisateur

---
+++ scripts/cli/users/create.php
@@ -0,0 +1,15 @@
+<?php
+
+if ($argc > 3) {
+    $u = new User();
+    $pwd = \Core\Text::random(10);
+    $u->set('firstname', $argv[1]);
+    $u->set('lastname', $argv[2]);
+    $u->set('email', $argv[3]);
+    $u->setPassword($pwd);
+    $u->save();
+    echo "Email    : {$argv[3]}\n";
+    echo "Password : {$pwd}\n";
+} else {
+    echo "Usage: php cli.php users/create firstname lastname email@domain.tld\n";
+}
